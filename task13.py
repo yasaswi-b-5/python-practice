e3cf90846f266ad2c33f2e869b8c18a6aa935bf1
Yasaswi Batta <yasaswibatta@gmail.com>
2026-08-08 16:21:32 +0530

    Add files via upload

---
+++ task13.py
@@ -0,0 +1,32 @@
+
+1.Write a Python program to separate the even and odd elements of a list into two different lists?
+Sample Input: 10 15 22 33 40 55
+Sample Output:
+Even List: 10 22 40
+Odd List: 15 33 55'''
+inp=[10,15,22,33,40,55]
+for i in  inp :
+    if i%2==0:
+        print('even list:',i)
+    else:
+        print('odd list:',i)
+
+
+
+
+'''2.Write a Python program to count the total number of prime numbers present in a list?
+Sample Input:[2 4 5 9 11 15]
+Sample Output:3
+
+3.Write a Python program to move all zero elements to the end of a list while maintaining the order of the remaining elements?
+Sample Input:[2 0 5 0 8 1 0]
+Sample Output:[2 5 8 1 0 0 0]
+
+4.Write a Python program to find the missing number in a list?
+Sample Input:[1 2 4 5]
+Sample Output:3
+
+5.Write a Python program to rotate the elements of a list one position to the left?
+
+Sample Input:[10 20 30 40 50]
+Sample Output:[20 30 40 50 10]'''
