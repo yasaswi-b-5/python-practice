e53299a574d12c0668039d7d0d3f1b0fa7a4a356
Yasaswi Batta <yasaswibatta@gmail.com>
2026-08-24 10:46:22 +0530

    Add files via upload

---
+++ sql3.sql
@@ -0,0 +1,6 @@
+select * from employee;
+select emp_name,emp_role from employee;
+select emp_name,emp_join_date from employee
+select emp_name,emp_no from employee
+select emp_name,emp_dept_id from employee
+select emp_name,emp_role from employee
